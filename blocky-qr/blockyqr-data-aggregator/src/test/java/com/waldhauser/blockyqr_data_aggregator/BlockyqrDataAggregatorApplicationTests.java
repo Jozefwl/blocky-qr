db8d88da9d9d3e437341526ebb8a681dafd02d52
Jozef Waldhauser <jozef.waldhauser@unicorn.com>
2026-03-14 09:18:43 +0100

    Add initial blockyqr-data-aggregator spring template

---
+++ blocky-qr/blockyqr-data-aggregator/src/test/java/com/waldhauser/blockyqr_data_aggregator/BlockyqrDataAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waldhauser.blockyqr_data_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockyqrDataAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
